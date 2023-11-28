ddac9d132cf431689106c7c1bf62ef0f08a65d84
kannarajesh <kannarajesh@live.com>
2023-11-28 13:21:05 +0000

    V28112023

--- EGIN/settings.py
+++ EGIN/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'EGIN.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/root/EGIN_MUSIC/MUSIC/templates/','/root/EGIN_MUSIC/EGIN/templates/'],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
